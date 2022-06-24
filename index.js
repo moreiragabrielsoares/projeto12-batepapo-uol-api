f31adf82f5b2d42a447b4fe1a9e42c1cde5127b3
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-06-24 12:12:31 -0300

    implementa o verbo Post na rota /participants

--- index.js
+++ index.js
@@ -12,6 +12,70 @@ server.use(express.json());
 server.use(cors());
 
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+async function connectDB () {
+    try {
+
+        await mongoClient.connect();
+        db = mongoClient.db('projeto12BatePapoUol');
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const participantNameSchema = joi.object({
+    name: joi.string().required(),
+});
+
+
+
+server.post('/participants', async (req , res) => {
+
+    await connectDB();
+
+    try {
+
+        const participant = req.body;
+        const validation = participantNameSchema.validate(participant, { abortEarly: true });
+
+        if (validation.error) {
+            res.sendStatus(422);
+            mongoClient.close();
+            return;
+        }
+
+        const participantsList = await db.collection('participants').find().toArray();
+
+        const found = participantsList.find(element => element.name === participant.name);
+
+        if (found) {
+            res.sendStatus(409);
+            mongoClient.close();
+            return;
+        }
+
+        const newParticipant = {name: participant.name, lastStatus: Date.now()}
+
+        await db.collection('participants').insertOne(newParticipant);
+
+        const time = dayjs(Date.now()).format('HH:mm:ss');
+        const loginMsg = {from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time};
+
+        await db.collection('messages').insertOne(loginMsg);
+        
+        res.sendStatus(201);
+        mongoClient.close();
+
+    } catch (error) {
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+
+});
 
 
 
@@ -19,8 +83,6 @@ server.use(cors());
 
 
 
-
-
-app.listen(5000, () => {
+server.listen(5000, () => {
     console.log('Server is litening on port 5000.');
 });
