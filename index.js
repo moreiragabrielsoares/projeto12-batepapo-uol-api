bc484593d9cdaf9125aa9f21b7eea2d85be5a42d
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-06-24 22:27:03 -0300

    implementa o verbo Post na rota /messages

--- index.js
+++ index.js
@@ -102,6 +102,58 @@ server.get('/participants' , async (req, res) => {
 
 
 
+server.post('/messages', async (req , res) => {
+
+    await connectDB();
+
+    try {
+
+        const user = req.headers.user;
+        
+        const message = {from: user, ... req.body};
+
+        const participantsList = await db.collection('participants').find().toArray();
+
+        const participants = participantsList.map(element => element.name);
+
+
+        const messageSchema = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().valid('message', 'private_message'),
+            from: joi.string().valid(... participants)
+        });
+
+        const validation = messageSchema.validate(message, { abortEarly: true });
+
+        console.log(validation.error);
+
+        if (validation.error) {
+            res.sendStatus(422);
+            mongoClient.close();
+            return;
+        }
+
+        const time = dayjs(Date.now()).format('HH:mm:ss');
+        
+        const newMessage = {... message, time: time};
+
+        await db.collection('messages').insertOne(newMessage);
+        
+        res.sendStatus(201);
+        mongoClient.close();
+
+    } catch (error) {
+        res.sendStatus(500);
+        mongoClient.close();
+    }
+
+});
+
+
+
+
+
 server.listen(5000, () => {
     console.log('Server is litening on port 5000.');
 });
